69e18781c52a7faae6f93a8a0bbfe3e053519c8c
Gerald Png <gpng@estl.sg>
2022-08-01 17:32:41 +0800

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <style global jsx>{`
+        body {
+          margin: 0;
+        }
+      `}</style>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
